f12816be82506cacd08c34f077edfe515f83e977
Michał Kucharski <michal.kucharski@techmo.pl>
2021-03-22 16:50:43 +0100

    correct links

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-[![pypi](https://img.shields.io/pypi/v/techmo.svg)](https://test.pypi.org/pypi/techmo)
-[![Supported Python versions](https://img.shields.io/pypi/pyversions/techmo.svg)](https://test.pypi.org/pypi/techmo)
+[![pypi](https://img.shields.io/pypi/v/techmo.svg)](https://pypi.org/pypi/techmo)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/techmo.svg)](https://pypi.org/pypi/techmo)
 ![example workflow](https://github.com/mikuchar/techmo/actions/workflows/python-publish.yml/badge.svg)
-## [Techmo Sp. z o.o.](http://techmp.pl) module for audio features extraction
+## [Techmo Sp. z o.o.](http://techmo.pl) module for audio features extraction
 
 ### How to use
 ```
--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 # coding=utf-8
 
 from pathlib import Path
-
 from setuptools import find_packages, setup
 
 project_root = Path(__file__).parent
-
 install_requires = (project_root / 'requirements.txt').read_text().splitlines()
 exec(open('techmo/_version.py').read())
+
 setup(name='techmo',
       version=__version__,
-      python_requires='>=3.5.0',
       url='https://github.com/mikuchar/techmo',
       author='Mariusz Ziółko, Michał Kucharski',
       author_email='mariusz.ziolko@techmo.pl',
@@ -18,4 +16,13 @@ setup(name='techmo',
       install_requires=install_requires,
       long_description=(project_root / 'README.md').read_text(),
       long_description_content_type='text/markdown',
+      python_requires='>=3.5.0',
+      classifiers=[
+          'Programming Language :: Python :: 3 :: Only'
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],
       packages=find_packages())
